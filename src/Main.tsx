49fe9ac105449e28d1d48196f94c5c4cd98bdbf0
yukishima-b <mashi.07.st@gmail.com>
2022-01-06 00:20:28 +0900

    Add base

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import {
@@ -8,32 +8,62 @@ import {
   Input,
   Button,
   Typography,
+  TextField,
 } from '@mui/material'
 
 const App = () => {
   const [username, setUsername] = useState('')
   const [currentStats, setCurrentStats] = useState('')
   const [currentTopLanguage, setCurrentTopLanguage] = useState('')
+  const [allHistory, setAllHistory] = useState<chrome.history.HistoryItem[]>([])
+  const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
     console.log('init')
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const currentURL = tabs[0].url || ''
-      console.log('currentUrl', currentURL)
+    chrome.history.search({ maxResults: 200, text: '' }, (historyItems) => {
+      console.log('history', historyItems)
+      setAllHistory(historyItems)
+    })
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      console.log('tabs', tabs)
     })
   }, [])
 
   useEffect(() => {}, [])
 
+  const results = useMemo(
+    () =>
+      allHistory
+        ? allHistory.filter(
+            (h) => h.title?.includes(searchText) || h.url?.includes(searchText)
+          )
+        : [],
+    [allHistory, searchText]
+  )
+
   return (
     <>
-      <Typography>{'test1111111!!!'}</Typography>
-      <Box width="540px" color={'red'}>
+      <Typography>{'rrrrr!!!'}</Typography>
+      <Box width="540px">
         <Box p={5}>
           <div dangerouslySetInnerHTML={{ __html: currentStats }} />
           <div dangerouslySetInnerHTML={{ __html: currentTopLanguage }} />
           <Typography>{'test1111111!!!'}</Typography>
         </Box>
+
+        <TextField
+          onChange={(event) => {
+            const value = event.target?.value
+            if (value) {
+              setSearchText(value)
+            }
+          }}
+        />
+
+        {`${results.length} / ${allHistory.length}`}
+
+        {JSON.stringify(results)}
+
         <Box pb={2} pl={4} pr={4}></Box>
       </Box>
     </>
